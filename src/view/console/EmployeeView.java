2841e3dd7c03d36446e3e7348ea6609b5fb9faab
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-04-08 17:00:29 -0300

    feat: created employee menu

---
+++ src/view/console/EmployeeView.java
@@ -0,0 +1,70 @@
+package view.console;
+
+import controller.PersonController;
+import model.people.Employee;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class EmployeeView {
+    private final Scanner scanner;
+    private final PersonController controller;
+
+    public EmployeeView(Scanner scanner, PersonController controller) {
+        this.scanner = scanner;
+        this.controller = controller;
+    }
+
+    public void menu() {
+        int option;
+        do {
+            System.out.println("\n--- MENU DE FUNCIONÁRIOS ---");
+            System.out.println("1. Cadastrar funcionário");
+            System.out.println("2. Listar funcionários");
+            System.out.println("0. Voltar");
+            System.out.print("Escolha a opção: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1 -> registerEmployee();
+                case 2 -> listEmployees();
+                case 0 -> System.out.println("Voltando ao menu principal...");
+                default -> System.out.println("Opção inválida.");
+            }
+        } while (option != 0);
+    }
+
+    private void registerEmployee() {
+        System.out.print("Nome: ");
+        String name = scanner.nextLine();
+
+        System.out.print("CPF: ");
+        String cpf = scanner.nextLine();
+
+        System.out.print("Data de Nascimento (AAAA-MM-DD): ");
+        LocalDate birthDate = LocalDate.parse(scanner.nextLine());
+
+        System.out.print("Cargo: ");
+        String position = scanner.nextLine();
+
+        System.out.print("Salario: ");
+        double salary = scanner.nextDouble();
+        scanner.nextLine();
+
+        LocalDate hireDate = LocalDate.now();
+
+        boolean active = true;
+
+        Employee employee = new Employee(name, cpf, birthDate, position, salary, hireDate, active);
+        controller.registerPerson(employee);
+
+        System.out.println("Funcionário cadastrado com sucesso!");
+    }
+
+
+    private void listEmployees() {
+        System.out.println("\n--- Lista de Funcionários ---");
+        controller.listEmployees().forEach(System.out::println);
+    }
+}
