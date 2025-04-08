3eb15d6a33486e37e287fed1ff76c810e71e6e61
FelipeFerraz4 <felipeferraz2402@gmail.com>
2025-04-08 16:59:29 -0300

    feat: created person controller

---
+++ src/controller/PersonController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import model.people.Person;
+import model.people.Guest;
+import model.people.Employee;
+import repository.person.PersonRepositoryHashMap;
+
+import java.util.List;
+import java.util.UUID;
+
+public class PersonController {
+    private final PersonRepositoryHashMap repository;
+
+    public PersonController() {
+        this.repository = new PersonRepositoryHashMap();
+    }
+
+    public void registerPerson(Person person) {
+        repository.add(person);
+    }
+
+    public List<Person> listAll() {
+        return repository.search();
+    }
+
+    public Person findById(UUID id) {
+        return repository.searchById(id);
+    }
+
+    public void updatePerson(Person updatedPerson) {
+        repository.update(updatedPerson);
+    }
+
+    public void deletePerson(UUID id) {
+        repository.delete(id);
+    }
+
+    public List<Person> listGuests() {
+        return repository.getByType(Guest.class);
+    }
+
+    public List<Person> listEmployees() {
+        return repository.getByType(Employee.class);
+    }
+}
